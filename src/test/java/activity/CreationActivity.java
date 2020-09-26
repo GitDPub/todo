b9b61b69e36e5a5b6fe6f370c1468d8b5ac2de45
“Dieter” <publicpracticas@gmail.com>
2020-09-26 17:24:14 -0400

    update selector for exit button

--- src/test/java/activity/CreationActivity.java
+++ src/test/java/activity/CreationActivity.java
@@ -19,7 +19,7 @@ public class CreationActivity {
         this.inputNotes = new TextBox(By.id("com.vrproductiveapps.whendo:id/noteTextNotes"));
         this.simpleCheckButton = new Button(By.id("com.vrproductiveapps.whendo:id/saveItem"));
         this.doubleCheckButton = new Button(By.id("com.vrproductiveapps.whendo:id/saveAndNew"));
-        this.exitButton = new Button(By.id("Navigate up"));
+        this.exitButton = new Button(By.xpath("//android.widget.ImageButton[@content-desc=\"Navigate up\"]"));
         this.checkBoxCompleted = new CheckBox(By.id("com.vrproductiveapps.whendo:id/note_item_marked"));
     }
 }
