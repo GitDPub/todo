75fc140168c343e4b87b9dac0f157a73c2b600fc
“Dieter” <publicpracticas@gmail.com>
2020-09-26 03:24:47 -0400

    Activity of task creation

---
+++ src/test/java/activity/CreationActivity.java
@@ -0,0 +1,25 @@
+package activity;
+
+import appiumControl.Button;
+import appiumControl.CheckBox;
+import appiumControl.TextBox;
+import org.openqa.selenium.By;
+
+public class CreationActivity {
+
+    public TextBox inputTitle;
+    public TextBox inputNotes;
+    public Button simpleCheckButton;
+    public Button doubleCheckButton;
+    public Button exitButton;
+    public CheckBox checkBoxCompleted;
+
+    public CreationActivity() {
+        this.inputTitle = new TextBox(By.id("com.vrproductiveapps.whendo:id/noteTextTitle"));
+        this.inputNotes = new TextBox(By.id("com.vrproductiveapps.whendo:id/noteTextNotes"));
+        this.simpleCheckButton = new Button(By.id("com.vrproductiveapps.whendo:id/saveItem"));
+        this.doubleCheckButton = new Button(By.id("com.vrproductiveapps.whendo:id/saveAndNew"));
+        this.exitButton = new Button(By.id("Navigate up"));
+        this.checkBoxCompleted = new CheckBox(By.id("com.vrproductiveapps.whendo:id/note_item_marked"));
+    }
+}
