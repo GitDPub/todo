9bbd6d85078952c30358e851b73fda1364c8a0e7
“Dieter” <publicpracticas@gmail.com>
2020-09-26 03:22:49 -0400

    Label added when only one task is in the list.

--- src/test/java/activity/MainActivity.java
+++ src/test/java/activity/MainActivity.java
@@ -1,13 +1,16 @@
 package activity;
 
 import appiumControl.Button;
+import appiumControl.Label;
 import org.openqa.selenium.By;
 
 public class MainActivity {
 
     public Button addButton;
+    public Label labelTitleOneTaskOnly;
 
     public MainActivity() {
         this.addButton = new Button(By.id("com.vrproductiveapps.whendo:id/fab"));
+        this.labelTitleOneTaskOnly = new Label(By.id("com.vrproductiveapps.whendo:id/home_list_item_text"));
     }
 }
