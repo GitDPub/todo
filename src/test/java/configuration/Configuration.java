9df0de5d445727666a1f5ef834330ac16897e0dc
“Dieter” <publicpracticas@gmail.com>
2020-09-30 21:47:32 -0400

    constants configurations

---
+++ src/test/java/configuration/Configuration.java
@@ -0,0 +1,7 @@
+package configuration;
+
+public class Configuration {
+    public static final String HOST = "https://todo.ly/";
+    public static final String BASIC_AUTHENTICATION = "Basic ZHBvcnRhbmRhQGdtYWlsLmNvbToxMjN0b2Rv";
+    public static final String BASIC_AUTHENTICATION_HEADER = "Authorization";
+}
