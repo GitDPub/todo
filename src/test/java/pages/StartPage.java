74e41bacf45a444399260f8d8b430958c464255d
“Dieter” <publicpracticas@gmail.com>
2020-10-05 23:32:40 -0400

    start page added

---
+++ src/test/java/pages/StartPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import control.Button;
+import org.openqa.selenium.By;
+
+public class StartPage {
+
+    public Button buttonGoToStart = new Button(By.xpath("//*[@id=\"top_bar_inner\"]/div[1]/button[2]"), "[Start] Button on start page");
+
+}
