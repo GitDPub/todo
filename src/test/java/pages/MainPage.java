92d62f4bc789e4d7a7c95eade5ed6b61a7a1daea
“Dieter” <publicpracticas@gmail.com>
2020-10-10 00:16:36 -0400

    locators fixed in pages

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.By;
 
 public class MainPage {
 
-    public Button buttonLogin = new Button(By.xpath("//a[@href=\"/users/showlogin\"]"), "[Login] Button on main page");
+    public Button buttonLogin = new Button(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/header/nav/div/ul[2]/li[1]/a"), "[Login] Button on main page");
 
 }
--- src/test/java/pages/StartPage.java
+++ src/test/java/pages/StartPage.java
@@ -4,7 +4,5 @@ import control.Button;
 import org.openqa.selenium.By;
 
 public class StartPage {
-
     public Button buttonGoToStart = new Button(By.xpath("//*[@id=\"top_bar_inner\"]/div[1]/button[2]"), "[Start] Button on start page");
-
 }
